88e206d3f6fea0ca081a45bf4d93ab6104b3be49
xuxinlei <381959437@qq.com>
2022-05-18 08:20:36 +0800

    Initial commit

---
+++ src/main/java/com/example/xudemo2/XuDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.xudemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xudemo2/XuDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xudemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
